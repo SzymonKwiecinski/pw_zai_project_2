6dc44a5f3979008137245f5c6d16d500f4776366
SzymonKwiecinski <kwiecinskji@gmail.com>
2023-11-20 22:37:31 +0100

    init

---
+++ src/data.js
@@ -0,0 +1,63 @@
+const events = [
+    {
+        name: "Planning for Q4",
+        startDate: "2023-08-01",
+        endDate: "2023-08-10",
+        description: "Collaborative session strategizing and aligning Q4 objectives for enhanced productivity. Detailed discussions on resource allocation, timelines, and KPIs resulted in a cohesive and energized plan.",
+        image: "",
+        category: "Work"
+    },
+    {
+        name: "Project Kickoff",
+        startDate: "2023-09-15",
+        endDate: "2023-09-20",
+        description: "Comprehensive project initiation with a focused kickoff meeting to set the tone and objectives. Team collaboration and coordination were emphasized for successful project implementation.",
+        image: "",
+        category: "Work"
+    },
+    {
+        name: "Team Building Workshop",
+        startDate: "2023-10-05",
+        endDate: "2023-10-07",
+        description: "Engaging work team members in a workshop designed to foster collaboration, communication, and unity. The interactive sessions aimed to strengthen team dynamics and boost morale.",
+        image: "",
+        category: "Work"
+    },
+    {
+        name: "Semester Start",
+        startDate: "2023-09-01",
+        endDate: "2023-09-10",
+        description: "Welcoming university students to the new semester with informative and engaging orientation events. The activities focused on acclimating students to campus life and academic expectations.",
+        image: "",
+        category: "University"
+    },
+    {
+        name: "Guest Lecture Series",
+        startDate: "2023-10-15",
+        endDate: "2023-10-25",
+        description: "Hosting industry experts for an enriching university lecture series, providing students with valuable insights and perspectives. The series aimed to broaden students' understanding of current industry trends.",
+        image: "",
+        category: "University"
+    },
+    {
+        name: "Research Symposium",
+        startDate: "2023-11-10",
+        endDate: "2023-11-15",
+        description: "Showcasing and discussing cutting-edge research in a university symposium. Researchers and academics convened to share findings, foster collaboration, and advance knowledge in various fields.",
+        image: "",
+        category: "University"
+    }
+];
+
+const categories = [
+    {
+        "name": "University",
+        "icon": "university.svg",
+        "color": "blue"
+    },
+    {
+        "name": "Work",
+        "icon": "work.svg",
+        "color": "red"
+    }
+]
