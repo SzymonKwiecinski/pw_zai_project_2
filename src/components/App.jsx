2848c2e233617707cc5d42c6f40c79eef5c7bbdf
SzymonKwiecinski <kwiecinskji@gmail.com>
2023-11-20 21:49:00 +0100

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Heading from "./Heading";
+import Footer from "./Footer";
+
+
+function App() {
+    return (
+        <div>
+            <Heading />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
